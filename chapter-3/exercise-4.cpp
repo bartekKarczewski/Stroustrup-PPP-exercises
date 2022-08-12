bbaf824a0155653f6f8ddc4b79d09f805c00c2b3
bartekKarczewski <karczewskiego@gmail.com>
2022-08-12 23:32:59 +0200

    first v of solution

--- chapter-3/exercise-4.cpp
+++ chapter-3/exercise-4.cpp
@@ -4,11 +4,39 @@
 #include "../std_lib_facilities.h"
 #include <iostream>
 
+void comparition(int val1, int val2){
+    if(val1 == val2) cout << "Integers are eqal\n";
+    if(val1 > val2) cout << val1 << " is bigger than " << val2 << "\n";
+    if(val1 < val2) cout << val2 << " is bigger than " << val1 << "\n";
+
+}
+
+int sum(int val1, int val2){
+    return val1 + val2;
+}
+
+int subtract(int val1, int val2){
+    return val1 - val2;
+}
+
+int multiply(int val1, int val2){
+    return val1 * val2;
+}
+
+double proportion(int val1, int val2){
+    return (val2 * 1.0)/ val1;
+}
+
 void init(){
     int val1{0};
     int val2{0};
     cout <<"Give me two integers: ";
     cin >> val1 >> val2;
+    comparition(val1, val2);
+    cout << "Sum is: " << sum(val1, val2) << "\n";
+    cout << "Difference is: " << subtract(val1,val2) << "\n";
+    cout << val1 << " times " << val2 << " equals: " << multiply(val1, val2) << "\n";
+    cout << val2 << " is " << proportion(val1, val2) << " of " << val1 << "\n";
 }
 
 int main(){
