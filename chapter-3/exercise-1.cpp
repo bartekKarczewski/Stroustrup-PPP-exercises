0697be78e394acf91cbe233fcc60a3393b8d66a1
bartekKarczewski <karczewskiego@gmail.com>
2022-08-12 21:13:46 +0200

    fixed exiting

--- chapter-3/exercise-1.cpp
+++ chapter-3/exercise-1.cpp
@@ -1,7 +1,16 @@
 /*Chapter 3 ** excercise 1:
     Programm converting miles to kilometeres */
+
 #include "../std_lib_facilities.h"
 #include <iostream>
+
+
+double calculate_km(double miles){
+    const double mil_to_km_ratio = 1.609;
+    double result{0};
+    result = miles * mil_to_km_ratio;
+    return result;
+}
 
 void init(){
     cout <<"**Welcome to miles to  kilometers converter**\n";
@@ -10,12 +19,15 @@ void init(){
     while(cin){
         cout << "Type number of miles: ";
         cin >> miles;
-        if(miles < 0) cout <<"Error: negative value typed\n";
+        if(miles < 0){
+            cout <<"Error: negative value typed\n";
+        }
+        if(miles > 0) cout << "Its "<< calculate_km(miles) << " kilometers :)" << endl;
+
     }
 }
 
 int main(){
     init();
-
     return 0;
 }
