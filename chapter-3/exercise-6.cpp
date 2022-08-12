292a37b77ad6bcacf2038ea6628ad556dbcdc664
bartekKarczewski <karczewskiego@gmail.com>
2022-08-13 00:40:22 +0200

    initial commit

---
+++ chapter-3/exercise-6.cpp
@@ -0,0 +1,24 @@
+/*Chapter 3 ** excercise 1:
+    Program recives 3 integers and sort them in ascending order */
+
+#include "../std_lib_facilities.h"
+#include <iostream>
+
+void sort(int val1, int val2, int val3){
+    int smallest{val1};
+    vector<int> values;
+}
+
+void init(){
+    int val1{0};
+    int val2{0};
+    int val3{0};
+    cout << "Give three integers to sort: ";
+    cin >> val1 >> val2 >> val3;
+}
+
+int main(){
+    init();
+
+    return 0;
+}
