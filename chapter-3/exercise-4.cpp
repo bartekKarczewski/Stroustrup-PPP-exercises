5d914d69c5240677053260818556744f50915177
bartekKarczewski <karczewskiego@gmail.com>
2022-08-12 21:21:52 +0200

    initial commit for ex.4

---
+++ chapter-3/exercise-4.cpp
@@ -0,0 +1,17 @@
+/*Chapter 3 ** excercise 1:
+    Program recives two integers and returns their comparision and some operations made on them(multiplication, division, proportion) */
+
+#include "../std_lib_facilities.h"
+#include <iostream>
+
+void init(){
+    int val1{0};
+    int val2{0};
+    cout <<"Give me two integers: ";
+    cin >> val1 >> val2;
+}
+
+int main(){
+    init();
+    return 0;
+}
