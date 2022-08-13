e07346347a4981c9fc2b5d358b2d10591b62875d
bartekKarczewski <karczewskiego@gmail.com>
2022-08-13 22:53:07 +0200

    solution with sort algorithm

--- chapter-3/exercise-6.cpp
+++ chapter-3/exercise-6.cpp
@@ -4,17 +4,24 @@
 #include "../std_lib_facilities.h"
 #include <iostream>
 
-void sort(int val1, int val2, int val3){
-    int smallest{val1};
-    vector<int> values;
-}
+void sort(vector<int> &values){
+    sort(values.begin(), values.end());
 
+    for(auto item: values){
+        cout << item << ", ";
+    }
+    
+}
 void init(){
+    vector<int> values;
     int val1{0};
     int val2{0};
     int val3{0};
     cout << "Give three integers to sort: ";
     cin >> val1 >> val2 >> val3;
+    values = {val1, val2, val3};
+    sort(values);
+
 }
 
 int main(){
