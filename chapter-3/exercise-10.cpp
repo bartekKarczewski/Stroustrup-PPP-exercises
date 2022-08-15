e0c23a246064a2e430204e872d833430aa40b2d5
bartekKarczewski <karczewskiego@gmail.com>
2022-08-15 21:29:40 +0200

    initial commit

---
+++ chapter-3/exercise-10.cpp
@@ -0,0 +1,46 @@
+/*Chapter 3 ** excercise 1:
+    Program recives operator with two numbers and returns result */
+
+#include "../std_lib_facilities.h"
+#include <iostream>
+
+
+void calculate(string operation, double val1, double val2){
+    if(operation == "plus" || operation == "+"){
+        cout << val1 + val2 << endl;
+    }
+    else if(operation == "minus" || operation == "-"){
+        cout << val1 - val2 << endl;
+    }
+    else if(operation == "times" || operation == "*"){
+        cout << val1 * val2 << endl;
+    }
+    else if(operation == "divide" || operation =="/"){
+        cout << val1 / val2 << endl;
+    }
+}
+
+
+void init(){
+    string operation;
+    double val1{0};
+    double val2{0};
+    while(cin){
+        cin >> operation >> val1 >> val2;
+        if(cin) calculate(operation, val1, val2);
+        else {
+            cout << "Invalid data\n";
+        }
+    }
+}
+
+int main()
+    try{
+        init();
+
+    return 0;
+}
+catch(runtime_error e){
+    cout << e.what() << endl;
+    keep_window_open("x");
+}
