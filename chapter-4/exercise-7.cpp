1146c5b432dec62460f2ffa31559ee140fdeab1e
bartekKarczewski <karczewskiego@gmail.com>
2022-08-27 18:17:14 +0200

    initial commit

---
+++ chapter-4/exercise-7.cpp
@@ -0,0 +1,52 @@
+/*Chapter 4 ** excercise 7:
+     Modify program from exercise 5 so that it accepts only single digits or spelled digits*/
+
+#include "../std_lib_facilities.h"
+#include <iostream>
+
+void calculate(double val1, double val2, char operation){
+    double result{0};
+    switch(operation){
+        case '+':
+            cout << "Sum of " << val1 << " and " << val2 << " is -> " << (val1 + val2) << endl;
+            break;
+        case '-':
+            cout << "Difference of " << val1 << " and " << val2 << " is -> " << (val1 - val2) << endl;
+            break;
+        case '*':
+            cout << "Multiplication of " << val1 << " and " << val2 << " is -> " << (val1 * val2) << endl;
+            break;
+        case '/':
+            if(val2 != 0){
+                cout << "Division of " << val1 << " and " << val2 << " is -> " << (val1 / val2) << endl;
+            }
+            else cout << "Can't divide by 0!" << endl;
+            break;
+        default:
+            cout << "Unknown operation" << endl;
+
+    }
+}
+
+void init(){
+    double val1{0};
+    double val2{0};
+    char operation;
+    cout << "**This is simple calculator**" << endl;
+    cout << "Enter two values and operation you wish to do" << endl;
+    cout << "Example: 0.4 2.5 *(each separated with space)" << endl;
+    cout << ">>";
+    cin >> val1 >> val2 >> operation;
+    calculate(val1, val2, operation);
+}
+
+int main()
+    try{
+        init();
+
+    return 0;
+}
+catch(runtime_error e){
+    cout << e.what() << endl;
+    keep_window_open("x");
+}
