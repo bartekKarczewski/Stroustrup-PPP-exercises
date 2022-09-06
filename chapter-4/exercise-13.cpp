8edba5631cb474e5a75610dfa670d1dd78fd04a1
bartekKarczewski <karczewskiego@gmail.com>
2022-09-06 11:47:49 +0200

    solution for ex 13

---
+++ chapter-4/exercise-13.cpp
@@ -0,0 +1,43 @@
+/*Chapter 4 ** excercise 13:
+     Program finds given number of primes and displays them*/
+
+#include "../std_lib_facilities.h"
+#include <iostream>
+
+bool is_prime(int num, vector<int> primes){
+    bool result = true;
+    for(auto prime: primes){
+        if(num % prime == 0) result = false; 
+    }
+    return result;
+}
+
+void display(vector<int> primes){
+    for(auto prime: primes) {
+        cout << prime << endl;
+    }
+}
+
+void init(){
+    vector<int> primes{2};
+    int count{0};
+    int num{3};
+    cout << "How many primes: ";
+    cin >> count;
+    while(primes.size() < count){
+        if(is_prime(num, primes)) primes.push_back(num);
+        num += 2;
+    }
+    display(primes);
+}
+
+int main()
+    try{
+        init();
+
+    return 0;
+}
+catch(runtime_error e){
+    cout << e.what() << endl;
+    keep_window_open("x");
+}
