a56b6841eb9a979f1be91c8afbc64ccba73216bd
bartekKarczewski <karczewskiego@gmail.com>
2022-09-11 20:56:29 +0200

    initial commit

---
+++ chapter-4/exercise-16.cpp
@@ -0,0 +1,16 @@
+/*Chapter 4 ** excercise 15:
+     Program searches for dominant in collection of numbers */
+
+#include "../std_lib_facilities.h"
+#include <iostream>
+
+int main()
+    try{
+        init();
+
+    return 0;
+}
+catch(runtime_error e){
+    cout << e.what() << endl;
+    keep_window_open("x");
+}
