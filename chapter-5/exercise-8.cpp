3cb479dfcae1fd3801246fbe82235b751a2d3098
bartekKarczewski <karczewskiego@gmail.com>
2022-09-26 23:16:20 +0200

    solution for ex 8

---
+++ chapter-5/exercise-8.cpp
@@ -0,0 +1,46 @@
+/*Chapter 5 ** excercise 8:
+    Program checks if delta is positive or negative and by that answers if equation with given values has solution*/
+
+#include "../std_lib_facilities.h"
+#include <iostream>
+
+void init();
+
+void sum_up(int size, vector<int> values){
+    if(size < values.size()){
+        int sum{0};
+        for(int i = 0; i < size; i++){
+            sum += values[i];
+        }
+        cout << "Requested sum equals: " << sum << endl;
+    }
+    else {
+        error("Not enough elements in collection");
+    }
+}
+
+void init(){
+    vector<int> values;
+    int size;
+    int temp;
+    cout << "How many numbers you wish to sum up: ";
+    cin >> size;
+    if(size < 1) error("Cannot sum up less than one value");
+    cout << "Enter couple numbers to collection(terminate with '|'): ";
+    while(cin >> temp){
+        values.push_back(temp);
+    }
+    sum_up(size, values);
+}
+
+int main(){
+    try{
+        init();
+    }
+    catch(runtime_error &e){
+        cout << e.what() << endl;
+        keep_window_open("x");
+    }
+
+    return 0;
+}
