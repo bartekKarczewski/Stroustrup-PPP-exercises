d38d536458e12462ac342ba367de59dfe6f9b453
bartekKarczewski <karczewskiego@gmail.com>
2022-11-27 22:49:10 +0100

    subtract fix

--- chapter-6/practice/practice-1.cpp
+++ chapter-6/practice/practice-1.cpp
@@ -160,7 +160,7 @@ double expression()
             t = ts.get();
             break;
         case '-':
-            left += term();    // evaluate Term and subtract
+            left -= term();    // evaluate Term and subtract
             t = ts.get();
             break;
         default:
